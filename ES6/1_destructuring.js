08ea8300825c9cd54525920746463c9d162dd9e5
Kunal-mob <102619984+Kunal-mob@users.noreply.github.com>
2025-02-20 22:30:02 +0530

    es6_destructuring

---
+++ ES6/1_destructuring.js
@@ -0,0 +1,13 @@
+const numbers = [10, 20, 30];
+
+let [a, b, c] = numbers;
+
+console.log(a); // 10
+console.log(b); // 20
+console.log(c); // 30
+const person = { name: "Alice", age: 25, city: "New York" };
+
+let { name, age } = person;  // Extracts properties
+
+console.log(name); // Alice
+console.log(age);  // 25
